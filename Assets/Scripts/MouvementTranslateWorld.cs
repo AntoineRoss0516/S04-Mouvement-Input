c8975d10d363377264ffd5405f640e15b4b85bda
AntoineRoss0516 <antoine.ross.16@outlook.com>
2025-02-12 16:55:33 -0500

    .

--- Assets/Scripts/MouvementTranslateWorld.cs
+++ Assets/Scripts/MouvementTranslateWorld.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class MouvementTranslateWorld : MonoBehaviour
 {
+    [SerializeField] private Vector3 _deplacement;
     void Update()
     {
-        transform.Translate(1f * Time.deltaTime, 0, 0, Space.World);
+        transform.Translate(0, 0, 1f * Time.deltaTime);
+        //Space.World(ChoseGlobale)ouSpace.Self(ChoseLocal)
+
+        //For Translate(), [SerializeField] et la déclarer*/
+        Vector3 deplacementCetFrame = _deplacement * Time.deltaTime;
+        transform.Translate(deplacementCetFrame.x, deplacementCetFrame.y, deplacementCetFrame.z, Space.Self);
+        //transform.Translate(deplacementCetFrame, Space.Self); Sert à affecter les trois directions sans le répéter.
 
         //Vector3 deplacement = new Vector3(1f, 0, 0);
         //transform.Translate(deplacement * Time.deltaTime, Space.World);
